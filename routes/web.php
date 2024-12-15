b99b1d140c71129986fb4e2f379350cddb930229
THORIQ\Thoriq <thoriq.6d18@gmail.com>
2024-12-15 21:25:18 +0700

    part 18

--- routes/web.php
+++ routes/web.php
@@ -63,4 +63,5 @@ Route::get('/dashboard', function()
 }
 )->Middleware('auth');
 
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
